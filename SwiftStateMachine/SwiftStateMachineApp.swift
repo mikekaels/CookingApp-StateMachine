71b7937563237106bc9bdb20bea750f8eb893cd1
MIke <santo.michael@privyid.tech>
2024-05-13 23:27:09 +0700

    Inital Commit

---
+++ SwiftStateMachine/SwiftStateMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftStateMachineApp.swift
+//  SwiftStateMachine
+//
+//  Created by Santo Michael on 13/05/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftStateMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
